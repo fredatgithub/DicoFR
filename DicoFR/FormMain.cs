78a8ecefb1cc066f7acf88b044a123154e71aadf
fred <domaine75@gmail.com>
2023-07-29 11:12:35 +0200

    Ajoutez des fichiers projet.

---
+++ DicoFR/FormMain.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DicoFR
+{
+  public partial class FormMain : Form
+  {
+    public FormMain()
+    {
+      InitializeComponent();
+    }
+
+    private void quitterToolStripMenuItem_Click(object sender, EventArgs e)
+    {
+      Application.Exit();
+    }
+
+    private void FormMain_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    private void ButtonExtract_Click(object sender, EventArgs e)
+    {
+
+    }
+  }
+}
